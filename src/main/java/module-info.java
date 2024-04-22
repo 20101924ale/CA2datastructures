13b45e1b3e5f0122763d75bf30612a59d77c07b4
20101924ale <114234258+20101924ale@users.noreply.github.com>
2024-04-22 14:02:13 +0100

    commit 2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,14 +2,8 @@ module com.example.parisroutefinder {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+    requires javafx.graphics;
+    requires java.desktop;
 
     opens com.example.parisroutefinder to javafx.fxml;
     exports com.example.parisroutefinder;
