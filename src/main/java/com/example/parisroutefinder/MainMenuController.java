1548802ab96e68b498c9f0eff8ff2c7ce804765a
20101924ale <114234258+20101924ale@users.noreply.github.com>
2024-04-23 16:46:36 +0100

    added pics and welcomemethods

--- src/main/java/com/example/parisroutefinder/MainMenuController.java
+++ src/main/java/com/example/parisroutefinder/MainMenuController.java
@@ -2,13 +2,17 @@ package com.example.parisroutefinder;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class HelloController {
+public class MainMenuController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextField nameField;
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Bienvenue," + nameField.getText() + "!");
     }
 }
